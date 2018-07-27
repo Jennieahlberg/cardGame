3bfa66753d5abce79d71b1df7a545420604f0ddf
Jennie Ahlberg <36049454+Jennieahlberg@users.noreply.github.com>
2018-07-27 13:55:48 +0200

    Changed the path to the text files

--- src/com/company/PlayingCardGame.java
+++ src/com/company/PlayingCardGame.java
@@ -14,15 +14,15 @@ public class PlayingCardGame {
         int menu = input.nextInt();
 
 //        while (menu != 1 || menu != 2 || menu != 3 || menu != 0) {
-            if (menu == 1) {
-                play();
-            } else if (menu == 2) {
-                rules();
-            } else if (menu == 3) {
-                highscore();
-            } else if (menu == 0) {
-                System.exit(1);
-            }
+        if (menu == 1) {
+            play();
+        } else if (menu == 2) {
+            rules();
+        } else if (menu == 3) {
+            highscore();
+        } else if (menu == 0) {
+            System.exit(1);
+        }
 //        }
     }
 
@@ -35,47 +35,52 @@ public class PlayingCardGame {
         System.out.println("Your name: ");
         String name = input.nextLine();
 
+        System.out.println("How many rounds do you wish to play?");
+        int numberOfRounds = input.nextInt();
+
         System.out.println(name + ", take the top card of the deck by pressing 1, to quit press 0.");
         int cardChoice = input.nextInt();
 
         int count = 0;
 
         if (cardChoice == 1) {
-//            for (int i = 0; i >= 10; i++) {
-            PlayingCard card = deck.getTopCard();
-            deck.removeCard(0);
-            System.out.println(card);
 
-            System.out.println("Press 1 to take your hidden card from the deck.");
-            int hiddenCardChoice = input.nextInt();
+            for (int i = 0; i < numberOfRounds; i++) {
 
-            if (hiddenCardChoice == 1) {
-                PlayingCard hiddenCard = deck.getTopCard();
+                PlayingCard card = deck.getTopCard();
                 deck.removeCard(0);
+                System.out.println(card);
 
-                System.out.println("Is your first card:\nHigher, press 1\nLower, press 2\nthan the hidden card?");
-                int guess = input.nextInt();
+                System.out.println("Press 1 to take your hidden card from the deck.");
+                int hiddenCardChoice = input.nextInt();
 
-                if (guess == 1) {
-                    if (card.getPrio() > hiddenCard.getPrio()) {
-                        System.out.println("Your card was higher than the hidden card. You get one point.\nThe hidden card was: " + hiddenCard);
-                        count++;
-                    } else if (card.getPrio() < hiddenCard.getPrio()) {
-                        System.out.println("Your card was lower than the hidden card. You get no point.\nThe hidden card was: " + hiddenCard);
-                    }
+                if (hiddenCardChoice == 1) {
+                    PlayingCard hiddenCard = deck.getTopCard();
+                    deck.removeCard(0);
 
-                } else if (guess == 2) {
-                    if (card.getPrio() < hiddenCard.getPrio()) {
-                        System.out.println("Your card was lower than the hidden card. You get one point.\nThe hidden card was: " + hiddenCard);
-                        count++;
-                    } else if (card.getPrio() > hiddenCard.getPrio()) {
-                        System.out.println("Your card was higher than the hidden card. You get no point.\nThe hidden card was: " + hiddenCard);
+                    System.out.println("Is your first card:\nHigher, press 1\nLower, press 2\nthan the hidden card?");
+                    int guess = input.nextInt();
+
+                    if (guess == 1) {
+                        if (card.getPrio() > hiddenCard.getPrio()) {
+                            System.out.println("Your card was higher than the hidden card. You get one point.\nThe hidden card was: " + hiddenCard);
+                            count++;
+                        } else if (card.getPrio() < hiddenCard.getPrio()) {
+                            System.out.println("Your card was lower than the hidden card. You get no point.\nThe hidden card was: " + hiddenCard);
+                        }
+
+                    } else if (guess == 2) {
+                        if (card.getPrio() < hiddenCard.getPrio()) {
+                            System.out.println("Your card was lower than the hidden card. You get one point.\nThe hidden card was: " + hiddenCard);
+                            count++;
+                        } else if (card.getPrio() > hiddenCard.getPrio()) {
+                            System.out.println("Your card was higher than the hidden card. You get no point.\nThe hidden card was: " + hiddenCard);
+                        }
                     }
                 }
-//                }
                 System.out.println(name + ", your total score is: " + count + " and is added to the highscore board. ");
 
-                writeToFile("C:\\Users\\Administrator\\OneDrive\\JavaDrive\\CardGame\\src\\com\\company\\highscore.txt", name, count);
+                writeToFile("textfiles\\highscore.txt", name, count);
 
             }
 
@@ -87,55 +92,14 @@ public class PlayingCardGame {
     }
 
     private void rules() {
-        try {
-            Scanner input = new Scanner(new File("C:\\Users\\Administrator\\OneDrive\\JavaDrive\\CardGame\\src\\com\\company\\rules.txt"));
-
-            while (input.hasNextLine()) {
-                String line = input.nextLine();
-                System.out.println(line);
-            }
-            input.close();
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        Scanner scan = new Scanner(System.in);
-        System.out.println("\nBack to menu, press 1\nQuit game, press 0");
-        int choice = scan.nextInt();
-
-        if (choice == 1) {
-            menu();
-        } else if (choice == 0) {
-            System.exit(1);
-        } else {
-            System.out.println("Error");
-        }
+        readFile("textfiles\\rules.txt");
+        backToMenu();
     }
 
     private void highscore() {
-        try {
-            Scanner input = new Scanner(new File("C:\\Users\\Administrator\\OneDrive\\JavaDrive\\CardGame\\src\\com\\company\\highscore.txt"));
+        readFile("textfiles\\highscore.txt");
+        backToMenu();
 
-            while (input.hasNextLine()) {
-                String line = input.nextLine();
-                System.out.println(line);
-            }
-            input.close();
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        Scanner scan = new Scanner(System.in);
-        System.out.println("\nBack to menu, press 1\nQuit game, press 0");
-        int chioce = scan.nextInt();
-
-        if (chioce == 1) {
-            menu();
-        } else if (chioce == 0) {
-            System.exit(1);
-        } else {
-            System.out.println("Error");
-        }
     }
 
     private void writeToFile(String fileName, String name, int count) {
@@ -151,5 +115,35 @@ public class PlayingCardGame {
             e.printStackTrace();
         }
 
+    }
+
+    private void readFile(String fileName) {
+        try {
+            Scanner input = new Scanner(new File(fileName));
+
+            while (input.hasNextLine()) {
+                String line = input.nextLine();
+                System.out.println(line);
+            }
+            input.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    private void backToMenu() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nBack to menu, press 1\nQuit game, press 0");
+        int chioce = scan.nextInt();
+
+        if (chioce == 1) {
+            menu();
+        } else if (chioce == 0) {
+            System.exit(1);
+        } else {
+            System.out.println("Error");
+        }
     }
 }
